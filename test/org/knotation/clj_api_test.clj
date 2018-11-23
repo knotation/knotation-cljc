71fbe5b5d1000f03b5f243e58c4cab1e3dc2c668
James A. Overton <james@overton.ca>
2018-11-23 09:24:40 -0500

    Add shared examples, improve CLJ API tests

--- test/org/knotation/clj_api_test.clj
+++ test/org/knotation/clj_api_test.clj
@@ -3,271 +3,70 @@
             [clojure.string :as string]
             [orchestra.spec.test :as stest]
 
-            [org.knotation.rdf :as rdf]
-            [org.knotation.environment :as en]
+            [org.knotation.examples :as ex]
             [org.knotation.state :as st]
-            [org.knotation.ttl :as ttl]
-            [org.knotation.kn :as kn]
             [org.knotation.clj-api :as api]
             [org.knotation.clj-api-spec]))
 
 (stest/instrument)
 
-(def test-0-kn
-  "@prefix kn: <https://knotation.org/kn/>
-@prefix ex: <http://example.com/>
+(defn kn->kn
+  [s]
+  (->> s
+       (api/read-string :kn nil)
+       (api/render-string :kn nil)))
 
-# comment
+(defn ttl->ttl
+  [s]
+  (->> s
+       (api/read-string :ttl nil)
+       st/sequential-blank-nodes
+       (api/render-string :ttl nil)))
 
-: ex:s
-ex:p; kn:link: ex:o
-ex:p: o
-ex:p; ex:d: o
-ex:p; @en: o
-")
+(defn kn->ttl
+  [s]
+  (->> s
+       (api/read-string :kn nil)
+       st/sequential-blank-nodes
+       (api/render-string :ttl nil)))
 
-(def test-0-ttl
-  "@prefix kn: <https://knotation.org/kn/> .
-@prefix ex: <http://example.com/> .
-
-ex:s
-  ex:p ex:o ;
-  ex:p \"o\" ;
-  ex:p \"o\"^^ex:d ;
-  ex:p \"o\"@en .
-")
-
-(def test-A-kn
-  "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-@prefix kn: <https://knotation.org/kn/>
-@prefix ex: <http://example.com/>
-
-: ex:p
-rdfs:label: p
-
-: ex:o
-rdfs:label: o
-
-: ex:s
-p; kn:link: o
-p: o
-p; ex:d: o
-p; @en: o
-")
-
-(def test-A-ttl
-  "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-@prefix kn: <https://knotation.org/kn/> .
-@prefix ex: <http://example.com/> .
-
-ex:p
-  rdfs:label \"p\" .
-
-ex:o
-  rdfs:label \"o\" .
-
-ex:s
-  ex:p ex:o ;
-  ex:p \"o\" ;
-  ex:p \"o\"^^ex:d ;
-  ex:p \"o\"@en .
-")
-
-(def test-1-kn
-  "@prefix kn: <https://knotation.org/kn/>
-@prefix ex: <http://example.com/>
-
-: ex:s
-ex:p; kn:link: ex:o
-> ex:a; kn:link: ex:b
-")
-
-(def test-1-ttl
-  "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix owl: <http://www.w3.org/2002/07/owl#> .
-@prefix ex: <http://example.com/> .
-
-ex:s
-  ex:p ex:o .
-
-_:b0
-  rdf:type owl:Annotation ;
-  owl:annotatedSource ex:s ;
-  owl:annotatedProperty ex:p ;
-  owl:annotatedTarget ex:o ;
-  ex:a ex:b .
-")
-
-(def test-2-kn
-  "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-@prefix owl: <http://www.w3.org/2002/07/owl#>
-@prefix ex: <http://example.com/>
-
-: ex:s
-ex:a: A
-> ex:b: B
->> ex:c: C
-> ex:d: D
-")
-
-(def test-2-ttl
-  "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix owl: <http://www.w3.org/2002/07/owl#> .
-@prefix ex: <http://example.com/> .
-
-ex:s
-  ex:a \"A\" .
-
-_:b0
-  rdf:type owl:Annotation ;
-  owl:annotatedSource ex:s ;
-  owl:annotatedProperty ex:a ;
-  owl:annotatedTarget \"A\" ;
-  ex:b \"B\" .
-
-_:b1
-  rdf:type owl:Annotation ;
-  owl:annotatedSource _:b0 ;
-  owl:annotatedProperty ex:b ;
-  owl:annotatedTarget \"B\" ;
-  ex:c \"C\" .
-
-_:b2
-  rdf:type owl:Annotation ;
-  owl:annotatedSource ex:s ;
-  owl:annotatedProperty ex:a ;
-  owl:annotatedTarget \"A\" ;
-  ex:d \"D\" .
-")
-
-(def test-3-kn
-  "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-@prefix owl: <http://www.w3.org/2002/07/owl#>
-@prefix kn: <https://knotation.org/kn/>
-@prefix ex: <http://example.com/>
-
-: ex:s
-ex:before: before
-ex:a; kn:anon:
- ex:b: B
- ex:c; kn:anon:
-  ex:d: D
- ex:e: E
-ex:after: after
-")
-
-(def test-3-ttl
-  "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix owl: <http://www.w3.org/2002/07/owl#> .
-@prefix kn: <https://knotation.org/kn/> .
-@prefix ex: <http://example.com/> .
-
-ex:s
-  ex:before \"before\" ;
-  ex:a [
-    ex:b \"B\" ;
-    ex:c [
-      ex:d \"D\"
-    ] ;
-    ex:e \"E\"
-  ] ;
-  ex:after \"after\" .
-")
-
-(def test-4-ttl
-  "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix ex: <http://example.com/> .
-
-ex:s
-  ex:p \"before\" ;
-  ex:p _:b0 ;
-  ex:p [
-    ex:p \"inner\" ;
-    ex:p [
-      ex:p \"innermost\"
-    ] ;
-    ex:p \"innerafter\"
-  ] ;
-  ex:p (
-    ex:a
-    ex:b
-    ex:c
-    ex:d
-  ) ;
-  ex:p (
-    ex:a
-    (
-      ex:b
-      ex:c
-    )
-    [
-      ex:p ex:x ;
-      ex:p ex:y
-    ]
-    ex:d
-  ) ;
-  ex:p \"after\" .
-")
+(defn ttl->kn
+  [s]
+  (->> s
+       (api/read-string :ttl nil)
+       st/sequential-blank-nodes
+       (api/render-string :kn nil)))
 
 (defn test-kn-roundtrip
   [s]
-  (->> s
-       (api/read-string :kn nil)
-       (kn/render-states st/default-state)
-       st/render-output-string
-       (= s)
-       is))
-
-(deftest test-kn
-  (test-kn-roundtrip test-0-kn)
-  (test-kn-roundtrip test-A-kn))
-  ;(test-kn-roundtrip test-1-kn))
+  (is (= s (kn->kn s))))
 
 (defn test-ttl-roundtrip
   [s]
-  (->> s
-       (api/read-string :ttl nil)
-       st/sequential-blank-nodes
-       (ttl/render-states en/default-env)
-       st/render-output-string
-       (= s)
-       is))
-
-(deftest test-ttl
-  (test-ttl-roundtrip test-0-ttl)
-  (test-ttl-roundtrip test-1-ttl)
-  (test-ttl-roundtrip test-2-ttl)
-  (test-ttl-roundtrip test-3-ttl)
-  (test-ttl-roundtrip test-4-ttl))
-
-(defn test-kn->ttl
-  [k t]
-  (->> k
-       (api/read-string :kn nil)
-       st/sequential-blank-nodes
-       (ttl/render-states en/default-env)
-       (map ::st/output)
-       (map ::st/content)
-       string/join
-       (= t)
-       is))
-
-(defn test-ttl->kn
-  [t k]
-  (->> t
-       (api/read-string :ttl nil)
-       (kn/render-states st/default-state)
-       st/render-output-string
-       (= k)
-       is))
+  (is (= s (ttl->ttl s))))
 
 (defn test-kn-ttl-roundtrip
   [k t]
-  (test-kn->ttl k t)
-  (test-ttl->kn t k))
+  (is (= t (kn->ttl k)))
+  (is (= k (ttl->kn t))))
+
+(deftest test-kn
+  (test-kn-roundtrip ex/basic-datatypes-kn)
+  (test-kn-roundtrip ex/basic-labels-kn)
+  (test-kn-roundtrip ex/anonymous-subjects-kn)
+  (test-kn-roundtrip ex/basic-annotations-kn)
+  (test-kn-roundtrip ex/nested-annotations-kn))
+
+(deftest test-ttl
+  (test-ttl-roundtrip ex/basic-datatypes-ttl)
+  (test-ttl-roundtrip ex/anonymous-subjects-ttl)
+  (test-ttl-roundtrip ex/mixed-lists-ttl)
+  (test-ttl-roundtrip ex/basic-annotations-ttl)
+  (test-ttl-roundtrip ex/nested-annotations-ttl))
 
 (deftest test-kn-ttl
-  (test-kn->ttl test-0-kn test-0-ttl)
-  (test-kn-ttl-roundtrip test-A-kn test-A-ttl)
-  (test-kn-ttl-roundtrip test-2-kn test-2-ttl)
-  (test-kn-ttl-roundtrip test-3-kn test-3-ttl))
+  (test-kn-ttl-roundtrip ex/basic-datatypes-kn     ex/basic-datatypes-ttl)
+  (test-kn-ttl-roundtrip ex/basic-labels-kn        ex/basic-labels-ttl)
+  (test-kn-ttl-roundtrip ex/anonymous-subjects-kn  ex/anonymous-subjects-ttl)
+  (test-kn-ttl-roundtrip ex/basic-annotations-kn   ex/basic-annotations-ttl)
+  (test-kn-ttl-roundtrip ex/nested-annotations-kn  ex/nested-annotations-ttl))
---
+++ test/org/knotation/examples.cljc
@@ -0,0 +1,206 @@
+(ns org.knotation.examples)
+
+; # Basic Datatypes
+
+(def basic-datatypes-kn
+  "@prefix kn: <https://knotation.org/kn/>
+@prefix ex: <http://example.com/>
+
+: ex:s
+ex:p; kn:link: ex:o
+ex:p: o
+ex:p; ex:d: o
+ex:p; @en: o
+")
+
+(def basic-datatypes-ttl
+  "@prefix kn: <https://knotation.org/kn/> .
+@prefix ex: <http://example.com/> .
+
+ex:s
+  ex:p ex:o ;
+  ex:p \"o\" ;
+  ex:p \"o\"^^ex:d ;
+  ex:p \"o\"@en .
+")
+
+; # Basic Labels
+
+(def basic-labels-kn
+  "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+@prefix kn: <https://knotation.org/kn/>
+@prefix ex: <http://example.com/>
+
+: ex:p
+rdfs:label: p
+
+: ex:o
+rdfs:label: o
+
+: ex:s
+p; kn:link: o
+p: o
+p; ex:d: o
+p; @en: o
+")
+
+(def basic-labels-ttl
+  "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix kn: <https://knotation.org/kn/> .
+@prefix ex: <http://example.com/> .
+
+ex:p
+  rdfs:label \"p\" .
+
+ex:o
+  rdfs:label \"o\" .
+
+ex:s
+  ex:p ex:o ;
+  ex:p \"o\" ;
+  ex:p \"o\"^^ex:d ;
+  ex:p \"o\"@en .
+")
+
+; # Anonymous Subjects
+
+(def anonymous-subjects-kn
+  "@prefix kn: <https://knotation.org/kn/>
+@prefix ex: <http://example.com/>
+
+: ex:s
+ex:before: before
+ex:a; kn:anon:
+ ex:b: B
+ ex:c; kn:anon:
+  ex:d: D
+ ex:e: E
+ex:after: after
+")
+
+(def anonymous-subjects-ttl
+  "@prefix kn: <https://knotation.org/kn/> .
+@prefix ex: <http://example.com/> .
+
+ex:s
+  ex:before \"before\" ;
+  ex:a [
+    ex:b \"B\" ;
+    ex:c [
+      ex:d \"D\"
+    ] ;
+    ex:e \"E\"
+  ] ;
+  ex:after \"after\" .
+")
+
+; # Basic Annotations
+
+(def basic-annotations-kn
+  "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+@prefix owl: <http://www.w3.org/2002/07/owl#>
+@prefix kn: <https://knotation.org/kn/>
+@prefix ex: <http://example.com/>
+
+: ex:s
+ex:p; kn:link: ex:o
+> ex:a; kn:link: ex:b
+")
+
+(def basic-annotations-ttl
+  "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix owl: <http://www.w3.org/2002/07/owl#> .
+@prefix kn: <https://knotation.org/kn/> .
+@prefix ex: <http://example.com/> .
+
+ex:s
+  ex:p ex:o .
+
+_:b0
+  rdf:type owl:Annotation ;
+  owl:annotatedSource ex:s ;
+  owl:annotatedProperty ex:p ;
+  owl:annotatedTarget ex:o ;
+  ex:a ex:b .
+")
+
+; # Nested Annotations
+
+(def nested-annotations-kn
+  "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+@prefix owl: <http://www.w3.org/2002/07/owl#>
+@prefix ex: <http://example.com/>
+
+: ex:s
+ex:a: A
+> ex:b: B
+>> ex:c: C
+> ex:d: D
+")
+
+(def nested-annotations-ttl
+  "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix owl: <http://www.w3.org/2002/07/owl#> .
+@prefix ex: <http://example.com/> .
+
+ex:s
+  ex:a \"A\" .
+
+_:b0
+  rdf:type owl:Annotation ;
+  owl:annotatedSource ex:s ;
+  owl:annotatedProperty ex:a ;
+  owl:annotatedTarget \"A\" ;
+  ex:b \"B\" .
+
+_:b1
+  rdf:type owl:Annotation ;
+  owl:annotatedSource _:b0 ;
+  owl:annotatedProperty ex:b ;
+  owl:annotatedTarget \"B\" ;
+  ex:c \"C\" .
+
+_:b2
+  rdf:type owl:Annotation ;
+  owl:annotatedSource ex:s ;
+  owl:annotatedProperty ex:a ;
+  owl:annotatedTarget \"A\" ;
+  ex:d \"D\" .
+")
+
+; # Mixed Lists
+
+(def mixed-lists-ttl
+  "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix ex: <http://example.com/> .
+
+ex:s
+  ex:p \"before\" ;
+  ex:p _:b0 ;
+  ex:p [
+    ex:p \"inner\" ;
+    ex:p [
+      ex:p \"innermost\"
+    ] ;
+    ex:p \"innerafter\"
+  ] ;
+  ex:p (
+    ex:a
+    ex:b
+    ex:c
+    ex:d
+  ) ;
+  ex:p (
+    ex:a
+    (
+      ex:b
+      ex:c
+    )
+    [
+      ex:p ex:x ;
+      ex:p ex:y
+    ]
+    ex:d
+  ) ;
+  ex:p \"after\" .
+")
